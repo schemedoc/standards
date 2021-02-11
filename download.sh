0366cb5185a64578e44034fd1eddc6055ca63ba6
Lassi Kortela <lassi@lassi.io>
2021-02-11 16:30:34 +0200

    Add R7RS spec

--- download.sh
+++ download.sh
@@ -8,3 +8,4 @@ $fetch r3rs.tar https://groups.csail.mit.edu/mac/ftpdir/scheme-reports/r3rs.tar
 $fetch r4rs.tar https://groups.csail.mit.edu/mac/ftpdir/scheme-reports/r4rs.tar
 $fetch r5rs.tar https://groups.csail.mit.edu/mac/ftpdir/scheme-reports/r5rs.tar
 $fetch r6rs.tar.gz http://www.r6rs.org/final/r6rs.tar.gz
+$fetch r7rs-small-spec.zip https://small.r7rs.org/attachment/r7rs-small-spec.zip
