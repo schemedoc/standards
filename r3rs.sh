2186202dc7112d99370ad3290caf42f50e3ff19f
Lassi Kortela <lassi@lassi.io>
2021-02-11 15:49:20 +0200

    Add r3rs conversion using latex209

---
+++ r3rs.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+set -eu
+cd "$(dirname "$0")"
+echo "Entering directory '$PWD'"
+exec </dev/null
+exec >r3rs.log 2>&1
+set -x
+shasum -c r3rs.sha.src
+rm -rf r3rs
+mkdir r3rs
+(cd r3rs && tar -xf ../r3rs.tar)
+(cd r3rs && latex209 r3rs.tex)
+test -s r3rs/r3rs.toc
+(cd r3rs && latex209 r3rs.tex)
+test -s r3rs/r3rs.dvi
+(cd r3rs && dvipdf r3rs.dvi)
+test -s r3rs/r3rs.pdf
+(cd r3rs && dvips r3rs.dvi)
+test -s r3rs/r3rs.ps
+mv -i r3rs/r3rs.pdf output/
+shasum -c r3rs.sha.dst
