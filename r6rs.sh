3063c8358c04a3e8350f7890438b5ac1bb8fdd3f
Lassi Kortela <lassi@lassi.io>
2021-02-11 21:05:10 +0200

    Get first build of r6rs working

---
+++ r6rs.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+set -eu
+cd "$(dirname "$0")"
+echo "Entering directory '$PWD'"
+exec </dev/null
+exec >r6rs.log 2>&1
+set -x
+shasum -c r6rs.sha.src
+rm -rf r6rs
+tar -xf r6rs.tar.gz
+patch -p 0 <r6rs.patch
+(cd r6rs/document && make report LATEX=pdflatex)
+mv -i r6rs/document/r6rs{,-app,-lib,-rationale}.pdf output/
+shasum -c r6rs.sha.dst
