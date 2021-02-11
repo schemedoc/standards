5bd7e923b56cdd227386ba26694369ab5d01860e
Lassi Kortela <lassi@lassi.io>
2021-02-11 15:22:03 +0200

    Add reproducible build of r5rs PDF

---
+++ r5rs.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+set -eu
+cd "$(dirname "$0")"
+echo "Entering directory '$PWD'"
+exec >r5rs.log 2>&1
+set -x
+shasum -c r5rs.sha.src
+rm -rf r5rs
+tar -xf r5rs.tar
+cp r5rs/r5rs.tex r5rs/r5rs.tex.orig
+patch -p 0 <r5rs.patch
+(cd r5rs && latex r5rs.tex)
+(cd r5rs && latex r5rs.tex)
+(cd r5rs && pdflatex r5rs.tex)
+(cd r5rs && dvips r5rs.dvi)
+mv -i r5rs/r5rs.pdf output/
+shasum -c r5rs.sha.dst
