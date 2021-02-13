f00e925c043da08b34379cd00d352dce46bdf4f5
Lassi Kortela <lassi@lassi.io>
2021-02-13 11:00:38 +0200

    Add errata-corrected-r7rs
    The patch is done via `git diff draft-10..errata "spec/*.tex"` in
    John's r7rs-spec repo. The errata branch in that repo may change in
    the future, but draft-10 corresponds to the official version of the
    standard and ought to remain the same.

--- r7rs.sh
+++ r7rs.sh
@@ -6,12 +6,18 @@ exec </dev/null
 exec >r7rs.log 2>&1
 set -x
 shasum -c r7rs.sha.src
-rm -rf r7rs
-mkdir r7rs
-(cd r7rs && unzip ../r7rs-small-spec.zip)
-mv r7rs/r7rs-small-spec/* r7rs/
-rmdir r7rs/r7rs-small-spec
-cp r7rs/r7rs.tex r7rs/r7rs.tex.orig
+extract() {
+    rm -rf r7rs
+    mkdir r7rs
+    (cd r7rs && unzip ../r7rs-small-spec.zip)
+    mv r7rs/r7rs-small-spec/* r7rs/
+    rmdir r7rs/r7rs-small-spec
+}
+extract
 (cd r7rs && make r7rs.pdf)
 mv -i r7rs/r7rs.pdf output/
 shasum -c r7rs.sha.dst
+extract
+(cd r7rs && patch -p 2 <../r7rs-errata.patch)
+(cd r7rs && make r7rs.pdf)
+mv -i r7rs/r7rs.pdf output/errata-corrected-r7rs.pdf
